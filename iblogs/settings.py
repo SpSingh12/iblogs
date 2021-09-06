e7f7f506c4fc28c52ba7dd355d89fe63ac30227e
SpSingh12 <suryapratapsingh3802@gmail.com>
2021-09-06 13:50:57 +0530

    new change

--- iblogs/settings.py
+++ iblogs/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
