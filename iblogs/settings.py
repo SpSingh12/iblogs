174f45d1086f05c409a23c4bbd7d3003cbee6f79
SpSingh12 <suryapratapsingh3802@gmail.com>
2021-09-06 14:35:06 +0530

    bbc

--- iblogs/settings.py
+++ iblogs/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME':  'iblogs',
         'USER': 'root',
         'PASSWORD': 'surya',
-        'HOST': 'herokuapp.com',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
